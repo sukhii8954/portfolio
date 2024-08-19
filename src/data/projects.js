4d810d1c4f0d9a31bfad779205243090f0574628
Sukhpreet singh <sukhpreet9808@gmail.com>
2024-08-19 20:36:49 +0530

    updated portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,24 +6,26 @@ export const projectsdata =
       "title": "Project A",
       "imageSrc": imgproj,
       "description": "This is a project made to learn the latest languages by building an app.",
-      "skills": ["React", "CSS","API" ],
-      "demo": "https://youtube-clone-fend.netlify.app/",
-      "source": "https://github.com/sukhii8954/youtube-clone.git"
+      "skills": ["React", "Tailwind","Rapid API" , "Javascript" ],
+      "demo": "https://srfitness24x7.netlify.app",
+      "source": "https://github.com/sukhii8954/fitness24x7.git"
     },
+
     {
       "title": "Project B",
       "imageSrc": imgproj,
       "description": "This is a project made to learn the latest languages by building an app.",
-      "skills": ["React", "API" , "CSS3"],
+      "skills": ["React", "Javascript" , "CSS3"],
       "demo": "https://mireactapp.netlify.app/",
       "source": "https://github.com/sukhii8954/Micloneapp.git"
     },
+
     {
       "title": "Project C",
       "imageSrc": imgproj,
       "description": "This is a project made to learn the latest languages by building an app.",
-      "skills": ["React", "BootStrap", "CSS3"],
-      "demo": "https://github.com/sukhii8954/resumerepo1.github.io.git",
-      "source": "https://github.com/sukhii8954/resumerepo1.github.io.git"
+      "skills": ["HTML5", "JavaScript", "CSS3"],
+      "demo": "https://sukhii8954.github.io/sidcup-golf/",
+      "source": "https://github.com/sukhii8954/sidcup-golf.git"
     }
   ]
