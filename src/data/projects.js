fd02278260782e3b7768adb7938cc466dcff2fdc
Sukhpreet singh <sukhpreet9808@gmail.com>
2024-10-08 17:26:10 +0530

    projects_updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,42 +6,24 @@ export const projectsdata =
       "title": "Project A",
       "imageSrc": imgproj,
       "description": "This is a project made to learn the latest languages by building an app.",
-<<<<<<< HEAD
       "skills": ["React", "Tailwind","API" ,"Next" ],
       "demo": "https://srrentalapartment.netlify.app",
       "source": "https://github.com/sukhii8954/property_rental_platform.git"
-=======
-      "skills": ["React", "Tailwind","Rapid API" , "Javascript" ],
-      "demo": "https://srfitness24x7.netlify.app",
-      "source": "https://github.com/sukhii8954/fitness24x7.git"
->>>>>>> 4d810d1c4f0d9a31bfad779205243090f0574628
     },
-
     {
       "title": "Project B",
       "imageSrc": imgproj,
       "description": "This is a project made to learn the latest languages by building an app.",
-<<<<<<< HEAD
       "skills": ["React", "API" , "CSS3" , "javascript"],
-=======
-      "skills": ["React", "Javascript" , "CSS3"],
->>>>>>> 4d810d1c4f0d9a31bfad779205243090f0574628
       "demo": "https://mireactapp.netlify.app/",
       "source": "https://github.com/sukhii8954/Micloneapp.git"
     },
-
     {
       "title": "Project C",
       "imageSrc": imgproj,
       "description": "This is a project made to learn the latest languages by building an app.",
-<<<<<<< HEAD
       "skills": ["React", "Rapid API", "Tailwind" , "Vite"],
       "demo": "https://fitness24x7app.netlify.app/",
       "source": "https://github.com/sukhii8954/fitness24x7.git"
-=======
-      "skills": ["HTML5", "JavaScript", "CSS3"],
-      "demo": "https://sukhii8954.github.io/sidcup-golf/",
-      "source": "https://github.com/sukhii8954/sidcup-golf.git"
->>>>>>> 4d810d1c4f0d9a31bfad779205243090f0574628
     }
   ]
